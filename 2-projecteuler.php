48bce7e82e281ad00c9670fb056ad4d5f1ac4354
Alexys Leclerc <alexys.leclerc@gmail.com>
2017-07-19 13:52:43 -0400

    Create 2-projecteuler.php

---
+++ 2-projecteuler.php
@@ -0,0 +1,15 @@
+<?php
+function EvenFibonacciNumbers() {
+    $sum = 0;
+    $add = 0;
+    $fibonacci = 1;
+    while ($fibonacci < 4000000) {
+        if (!($fibonacci % 2)) {
+            $sum += $fibonacci;
+        }
+        $temp = $fibonacci + $add;
+        $add = $fibonacci;
+        $fibonacci = $temp;
+    }
+    return $sum;
+}
