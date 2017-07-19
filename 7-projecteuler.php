f2fc3df8a21e9ac1f65d9f738b76d9a550407913
Alexys Leclerc <alexys.leclerc@gmail.com>
2017-07-19 15:04:08 -0400

    Create 7-projecteuler.php

---
+++ 7-projecteuler.php
@@ -0,0 +1,27 @@
+<?php
+function isPrime($number) {
+    $i = 2;
+    $notFound = TRUE;
+    while ($i < $number && $notFound) {
+        if ($number % $i) {
+            $i++;
+        } else {
+            return FALSE;
+        }
+    }
+    return TRUE;
+}
+
+function checkForNPrime($primeNumber) {
+    $i = 0;
+    $numberOfPrime = -1; //remove 1
+    $lastPrime = 0;
+    while($numberOfPrime <= $primeNumber) {
+        if(isPrime($i)){
+            $lastPrime = $i;
+            $numberOfPrime++;
+        }
+        $i++;
+    }
+    return $lastPrime;
+}
