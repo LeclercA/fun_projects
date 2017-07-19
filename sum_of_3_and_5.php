1a980dcdaa1a14e1869da78becb0f9390f6c29d6
Alexys Leclerc <alexys.leclerc@gmail.com>
2017-07-19 13:29:05 -0400

    Create sum_of_3_and_5.php

---
+++ sum_of_3_and_5.php
@@ -0,0 +1,9 @@
+function getSum($number = 1000) {
+    $sum = 0;
+    for ($i = 0; $i < $number; $i++) {
+        if (!($i % 3) || !($i % 5)) {
+            $sum += $i;
+        }
+    }
+    return $sum;
+}
