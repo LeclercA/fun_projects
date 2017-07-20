b94a9d1db9b5bdae3aaea29a8bfbeb98416972f9
Alexys Leclerc <alexys.leclerc@gmail.com>
2017-07-20 08:07:32 -0400

    it works now

--- maze_generator.php
+++ maze_generator.php
@@ -1,18 +1,16 @@
 <?php
-class Cell
-{
+class Cell {
+
     public $x;
     public $y;
     public $visited = FALSE;
 
-    public function __construct(int $x, int $y)
-    {
+    public function __construct($x, $y) {
         $this->x = $x;
         $this->y = $y;
     }
 
-    protected function findNeighbours(Cell $cell, int $rows, int $columns)
-    {
+    protected function findNeighbours($cell, $rows, $columns) {
         $x = $cell->x;
         $y = $cell->y;
 
@@ -28,18 +26,19 @@ class Cell
         if ($x !== 0) {
             $cell->neighbours .= "W";
         }
-
     }
+
 }
 
-class Grid extends Cell
-{
+class Grid extends Cell {
+
     public $rows = 10;
     public $columns = 10;
     public $cells = [];
     public $lastVisited = [];
     public $actualMaze = [];
     public $road = [];
+
     const DIRECTION = [
         "E" => ["x" => 1, "y" => 0],
         "N" => ["x" => 0, "y" => -1],
@@ -48,70 +47,56 @@ class Grid extends Cell
     ];
     const REVERSE = ["E" => "W", "W" => "E", "S" => "N", "N" => "S"];
 
-
-    public function __construct(int $rows, int $columns)
-    {
+    public function __construct($rows, $columns) {
         $this->rows = $rows;
         $this->columns = $columns;
         $this->createGrid();
     }
 
-    private function createGrid()
-    {
+    private function createGrid() {
         for ($i = 0; $i < $this->rows; $i++) {
             for ($j = 0; $j < $this->columns; $j++) {
                 $this->cells[$i][$j] = new Cell($i, $j);
-
             }
         }
     }
 
-    public function createMaze(int $startX = 0, int $startY = 0)
-    {
+    public function createMaze($startX = 0, $startY = 0) {
         $numberOfCellsNotVisited = $this->rows * $this->columns;
         $currentCell = $this->cells[$startX][$startY];
-        while ($numberOfCellsNotVisited) {
-
+        $this->actualMaze[] = $currentCell;
+        $this->lastVisited[] = $currentCell;
+        while ($numberOfCellsNotVisited - 1) {
+            $currentCell->visited = TRUE;
             $availableNeighbours = NULL;
             $x = $currentCell->x;
             $y = $currentCell->y;
-            if ($x !== $this->rows - 1 && !$this->cells[$x + 1][$y]->visited) {
+            if ($x < $this->rows - 1 && !$this->cells[$x + 1][$y]->visited) {
                 $availableNeighbours .= "E";
             }
-            if ($y !== 0 && !$this->cells[$x][$y - 1]->visited) {
+            if ($y > 0 && !$this->cells[$x][$y - 1]->visited) {
                 $availableNeighbours .= "N";
             }
-            if ($y !== $this->columns - 1 && !$this->cells[$x][$y + 1]->visited) {
+            if ($y < $this->columns - 1 && !$this->cells[$x][$y + 1]->visited) {
                 $availableNeighbours .= "S";
             }
-            if ($x !== 0 && !$this->cells[$x - 1][$y]->visited) {
+            if ($x > 0 && !$this->cells[$x - 1][$y]->visited) {
                 $availableNeighbours .= "W";
             }
-            var_dump($availableNeighbours);
             if ($availableNeighbours) {
                 $randomChar = strlen($availableNeighbours) === 1 ? $availableNeighbours : $availableNeighbours[rand(0, strlen($availableNeighbours) - 1)];
+                $currentCell = $this->cells[$x + self::DIRECTION[$randomChar]["x"]][$y + self::DIRECTION[$randomChar]["y"]];
                 $this->actualMaze[] = $currentCell;
                 $this->lastVisited[] = $currentCell;
-                $currentCell->visited = TRUE;
-                $currentCell = $this->cells[$x + self::DIRECTION[$randomChar]["x"]][$y + self::DIRECTION[$randomChar]["y"]];
                 $numberOfCellsNotVisited--;
-            }
-            else {
-                $currentCell = end($this->lastVisited);
-
+            } else {
+                $currentCell = array_pop($this->lastVisited);
             }
             $this->road[] = $currentCell;
         }
-        //print_r($this->actualMaze);
-        //print_r($this->lastVisited);
-
-
-
-
     }
 
 }
-
 
 $grid = new Grid(10, 10);
 $grid->createMaze();
